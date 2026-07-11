9ac4f9927a384ebacf3306e63e588d5f2beff002
Claee <sewaku23@gmail.com>
2026-07-12 03:22:59 +0700

    feat(catalog): add pagination

--- catalog/views.py
+++ catalog/views.py
@@ -2,32 +2,38 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+from django.core.paginator import Paginator
 
 def product_list(request):
     """
-    Menampilkan daftar produk dan mendukung pencarian.
+    Menampilkan daftar produk beserta fitur pencarian dan pagination.
     """
 
     query = request.GET.get("q", "")
 
-    products = Product.objects.select_related(
-        "category"
-    )
+    products = Product.objects.select_related("category")
 
     if query:
         products = products.filter(
             name__icontains=query
         )
 
+    paginator = Paginator(products, 5)
+
+    page_number = request.GET.get("page")
+
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        "products": products,
+        "page_obj": page_obj,
+        "products": page_obj,
         "query": query,
     }
 
     return render(
         request,
         "catalog/product_list.html",
-        context
+        context,
     )
 
 def product_detail(request, pk):
