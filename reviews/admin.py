2e0c7cd9a2cdf5ba6883d7d653f0e7efff83cb8b
Claee <sewaku23@gmail.com>
2026-07-25 16:20:19 +0700

    feat(review): create review model and admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "user",
+        "get_product",
+        "rating",
+        "created_at",
+    )
+
+    list_filter = (
+        "rating",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "rental_item__product__name",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+    @admin.display(description="Product")
+    def get_product(self, obj):
+        return obj.rental_item.product.name
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,56 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from rentals.models import RentalItem
+
+
+class Review(models.Model):
+    """
+    Review dan rating untuk setiap produk
+    yang telah disewa pengguna.
+    """
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+
+    rental_item = models.OneToOneField(
+        RentalItem,
+        on_delete=models.CASCADE,
+        related_name="review",
+    )
+
+    rating = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5),
+        ],
+        verbose_name="Rating",
+    )
+
+    comment = models.TextField(
+        verbose_name="Review"
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
+
+    def __str__(self):
+        return (
+            f"{self.user.username} | "
+            f"{self.rental_item.product.name} | "
+            f"{self.rating}⭐"
+        )
